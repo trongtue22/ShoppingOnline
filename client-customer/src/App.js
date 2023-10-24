352bf40a2ff077968bb8d69c0ed05aaa60f6dded
trongtue22 <121679924+trongtue22@users.noreply.github.com>
2023-10-24 21:15:32 +0700

    Create App.js

---
+++ client-customer/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+         <Main/>
+      </BrowserRouter>
+    );
+  }
+}
+export default App;
