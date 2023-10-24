b360b42eebd257aa07f9b6e642cec82c7c5e1928
trongtue22 <121679924+trongtue22@users.noreply.github.com>
2023-10-24 21:08:06 +0700

    Update ProductDAO.js

--- server/models/ProductDAO.js
+++ server/models/ProductDAO.js
@@ -1,4 +1,5 @@
 
+
 require('../utils/MongooseUtil');
 const Models = require('./models');
 
@@ -25,7 +26,46 @@ const ProductDAO = {
   async delete(_id) {
         const result = await Models.Product.findByIdAndRemove(_id);
         return result;
-      }
+      },
+
+      async selectByID(_id) {
+        const product = await Models.Product.findById(_id).exec();
+        return product;
+      },
+      
+      async selectTopNew(top) {
+        const query = {};
+        const mysort = { cdate: -1 }; // descending
+        const products = await Models.Product.find(query).sort(mysort).limit(top).exec();
+        return products;
+      },
+      async selectTopHot(top) {
+        const items = await Models.Order.aggregate([
+          { $match: { status: 'APPROVED' } },
+          { $unwind: '$items' },
+          { $group: { _id: '$items.product._id', sum: { $sum: '$items.quantity' } } },
+          { $sort: { sum: -1 } }, // descending
+          { $limit: top }
+        ]).exec();
+        var products = [];
+        for (const item of items) {
+          const product = await ProductDAO.selectByID(item._id);
+          products.push(product);
+        }
+        return products;
+      },
+      
+      async selectByCatID(_cid) {
+        const query = { 'category._id': _cid };
+        const products = await Models.Product.find(query).exec();
+        return products;
+      },
+
+      async selectByKeyword(keyword) {
+        const query = { name: { $regex: new RegExp(keyword, "i") } };
+        const products = await Models.Product.find(query).exec();
+        return products;
+      },
 };
 
 module.exports = ProductDAO;
