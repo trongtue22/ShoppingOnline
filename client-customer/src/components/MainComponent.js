8568b2dc832762aa0de9625d682696a570b3a784
trongtue22 <121679924+trongtue22@users.noreply.github.com>
2023-11-02 16:46:41 +0700

    Update MainComponent.js

--- client-customer/src/components/MainComponent.js
+++ client-customer/src/components/MainComponent.js
@@ -1,12 +1,57 @@
+// // import React, { Component } from 'react';
+// // import Menu from './MenuComponent';
+// // import Inform from './InformComponent';
+// // import Home from './HomeComponent';
+
+// // class Main extends Component {
+// //   render() {
+// //     return (
+// //       <div className="body-customer">
+// //         <Menu />
+// //         <Inform />
+// //         <Home />
+// //       </div>
+// //     );
+// //   }
+// // }
+// // export default Main;
+
+
+
+// import React, { Component } from 'react';
+// import Menu from './MenuComponent';
+// import Inform from './InformComponent';
+// import Home from './HomeComponent';
+
+// class Main extends Component {
+//   render() {
+//     return (
+//       <div className="body-customer">
+//         <Menu />
+//         <Inform />
+//         <Home />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Main;
+
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Inform from './InformComponent';
 import Home from './HomeComponent';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Product from './ProductComponent';
-// import Product from './ProductComponent';
-// import ProductDetail from './ProductDetailComponent';
 import ProductDetail from './ProductDetailComponent';
+// LAB 6 
+import Signup from './SignupComponent';
+import Active from './ActiveComponent';
+import Login from './LoginComponent';
+import Myprofile from './MyprofileComponent';
+// lab-7:
+import Mycart from './MycartComponent';
+import Myorders from './MyordersComponent';
 class Main extends Component {
     render() {
         return (
@@ -16,10 +61,19 @@ class Main extends Component {
                 <Routes>
                     <Route path='/' element={<Navigate replace to='/home' />} />
                     <Route path='/home' element={<Home />} />
-
                     <Route path='/product/category/:cid' element={<Product/>}/>
                     <Route path='/product/search/:keyword' element={<Product/>}/>
                     <Route path ='/product/:id' element ={< ProductDetail/>}/>
+                    
+                    <Route path='/signup' element={<Signup/>}/>
+                    <Route path='/active' element={<Active/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/myprofile' element={<Myprofile/>}/>
+
+                    
+                    <Route path='/mycart' element={<Mycart />}/>
+                    <Route path='/myorders' element={<Myorders/>}/>
+
                 </Routes>
             </div>
         );
